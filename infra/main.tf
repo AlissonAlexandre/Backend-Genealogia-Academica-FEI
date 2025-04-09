ea0fa607e75f0468dd1dae5623c26be1bc7f4fb7
Alisson Alexandre Botelho Barros <botelho.alissonalexandre@gmail.com>
2025-04-08 23:14:03 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,14 +2,16 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.70.0"
+      version = "~> 3.90.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-}
+
+  skip_provider_registration = true
+}    
 
 resource "azurerm_resource_group" "main" {
   name     = "genealogia-academica"
