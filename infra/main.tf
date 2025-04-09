a50c7676093f22a34d0983b733d7adf159c7b4e6
Alisson Alexandre Botelho Barros <botelho.alissonalexandre@gmail.com>
2025-04-08 22:16:55 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_container_app_environment" "env" {
-  name                = "genealogia-env"
+  name                = "PROD"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
